63183dc48b180c9856d33b6755473bc8c5665387
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-01-30 16:18:12 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,47 +1,59 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) {
+public class Main {
 
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        Integer[] card = new Integer[n];
-        int total = 0;
+    static int[] cards;
+    static int n, m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        cards = new int[n];
+        st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < n; i++) {
-            card[i] = scanner.nextInt();
+            cards[i] = Integer.parseInt(st.nextToken());
         }
 
-        int result = 0;
+        binarySearchSolve();
+    }
 
-        // 3개를 고르기 때문에 첫번째 카드는 N - 2 까지만 순회
+    private static void binarySearchSolve() {
+
+        int left;
+        int mid ;
+        int right;
+        int max = 0;
+
+        Arrays.sort(cards);
         for (int i = 0; i < n - 2; i++) {
 
-            // 두 번째 카드는 첫 번째 카드 다음부터 N - 1 까지만 순회
-            for (int j = i + 1; j < n - 1; j++) {
+            left = i;
+            mid = i + 1;
+            right = n - 1;
 
-                // 세 번째 카드는 두 번째 카드 다음부터 N 까지 순회
-                for (int k = j + 1; k < n; k++) {
+            while (mid < right) {
 
-                    // 3개 카드의 합 변수 temp
-                    int temp = card[i] + card[j] + card[k];
+                int sum = cards[left] + cards[mid] + cards[right];
+                if (sum <= m && sum > max) {
+                    max = sum;
+                }
 
-                    // M과 세 카드의 합이 같다면 temp return 및 종료
-                    if (m == temp) {
-                        System.out.println(temp);
-                        return;
-                    }
-
-                    // 세 카드의 합이 이전 합보다 크면서 M 보다 작을 경우 result 갱신
-                    if(result < temp && temp < m) {
-                        result = temp;
-                    }
+                if (sum > m) {
+                    right--;
+                }
+                else {
+                    mid++;
                 }
             }
         }
-        System.out.println(result);
+        System.out.println(max);
     }
-
-
 }
