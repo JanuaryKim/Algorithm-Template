d8edee5b0acb0b8a7ef6eab8b2a49cf57e8400b4
JanuaryKim <unool6976@gmail.com>
2023-02-07 23:18:38 +0900

    BOJ : 1931 Greedy 회의실배정(수정)
    - 풀이 변경
    - 주석 추가

--- src/baekjoon/greedy_그리디/B1931_회의실배정.java
+++ src/baekjoon/greedy_그리디/B1931_회의실배정.java
@@ -5,47 +5,58 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class B1931_회의실배정{
+/** 성공 **/
+public class B1931_회의실배정 {
+    static int[][] meetings;
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //문제의 핵심
+        //일찍 종료되는 회의순으로 정렬한다.
+        //회의가 일찍 종료될 수록 많은 회의실이 비는 시간이 더 많아지므로, 가능한 회의수가 증가하게 된다.
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-
-        int[][] time = new int[N][2];
-
+        int n = Integer.parseInt(bf.readLine());
+        meetings = new int[n][];
         StringTokenizer st;
-
-        for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            time[i][0] = Integer.parseInt(st.nextToken());	// 시작시간
-            time[i][1] = Integer.parseInt(st.nextToken());	// 종료시간
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(bf.readLine());
+            meetings[i] = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
         }
 
-        Arrays.sort(time, new Comparator<int[]>() {
-
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[1] == o2[1]) {
-                    return o1[0] - o2[0];
-                }
-
-                return o1[1] - o2[1];
+        Arrays.sort(meetings, (m1, m2) -> {
+            //종료 시간이 같으면 시작 시간이 빠른것을 앞쪽으로 정렬 해줘야함
+            //그렇지 않으면 아래와 같은 반례에서 예외 발생
+            //3
+            //1 3
+            //8 8
+            //4 8
+            //인 상황에서 (8 8) 과 (4 8)을 swap 시키지 않으면
+            //가능한 회의는 2개밖에 안될것이다
+            //하지만
+            //3
+            //1 3
+            //4 8
+            //8 8
+            //이렇게 정렬을 해준다면
+            //3개의 회의가 가능해진다.
+            if(m1[1] == m2[1])
+            {
+                return m1[0] - m2[0];
             }
 
-        });
+            return m1[1] - m2[1];
+        } );
 
-        int count = 0;
-        int prev_end_time = 0;
-
-        for(int i = 0; i < N; i++) {
-
-            if(prev_end_time <= time[i][0]) {
-                prev_end_time = time[i][1];
-                count++;
+        int ctn = 0;
+        int preEnd = 0;
+        for (int i = 0; i < n; i++) {
+            if(meetings[i][0] >= preEnd)
+            {
+                ctn++;
+                preEnd = meetings[i][1];
             }
         }
-        System.out.println(count);
-
+        System.out.println(ctn);
     }
+
 }
