bf12335ff575bc95a6d4198d3f7e268783bc6b77
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-04-23 23:58:48 +0900

    [Silver IV] Title: 토너먼트, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.java
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -1,63 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = bf.readLine().split(" ");	// 참가자 수
+        int jimin = Integer.parseInt(inputs[1]);    // 김지민 번호
+        int hansoo = Integer.parseInt(inputs[2]);	// 임한수 번호
+        int count = 0;
 
-        String[] inputs = bf.readLine().split(" ");
-
-        int n = Integer.parseInt(inputs[0]);
-        int jimin = Integer.parseInt(inputs[1]);
-        int hansoo = Integer.parseInt(inputs[2]);
-
-        List<Integer> pre = new ArrayList<>();
-
-        for (int i = 1; i <= n; i++) {
-            pre.add(i);
+        while(jimin != hansoo) {
+            jimin = (jimin + 1) / 2;
+            hansoo = (hansoo + 1) / 2;
+            count  ++;
         }
-
-
-        int depth = 0;
-        while (pre.size() > 1) {
-
-            depth++;
-            List<Integer> next = new ArrayList<>();
-            List<Integer> temp = new ArrayList<>();
-
-            for (int i = 0; i < pre.size(); i++) {
-                Integer current = pre.get(i);
-                temp.add(current);
-
-                if(temp.size() == 2)
-                {
-                    if (temp.contains(jimin) && temp.contains(hansoo)) {
-                        System.out.println(depth);
-                        return;
-                    } else if (temp.contains(jimin)) {
-                        next.add(jimin);
-                    } else if (temp.contains(hansoo)) {
-                        next.add(hansoo);
-                    } else{
-                        next.add(temp.get(0));
-                    }
-
-                    temp.clear();
-                }
-            }
-
-            if(!temp.isEmpty())
-                next.add(temp.get(0));
-
-            pre = new ArrayList<>(next);
-            next.clear();
-        }
-
-        System.out.println(-1);
+        System.out.println(count);
     }
 }
