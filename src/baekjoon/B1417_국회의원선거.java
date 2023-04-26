3eba0eee641cce8d61e91e5a27f3d51c673e0918
JanuaryKim <unool6976@gmail.com>
2023-04-26 23:59:30 +0900

    BOJ : 1417 국회의원선거

---
+++ src/baekjoon/B1417_국회의원선거.java
@@ -0,0 +1,40 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class B1417_국회의원선거 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bf.readLine());
+
+        List<Integer> list = new LinkedList<>();
+
+        int dasom = Integer.parseInt(bf.readLine());
+
+        for (int i = 1; i < n; i++) {
+            int value = Integer.parseInt(bf.readLine());
+            list.add(value);
+        }
+
+        Collections.sort(list);
+        int result = 0;
+        while (dasom <= list.get(list.size()-1)) {
+
+            result++;
+            int value = list.get(list.size()-1);
+            dasom++;
+            list.remove(list.size()-1);
+            list.add(value-1);
+            Collections.sort(list);
+        }
+
+        System.out.println(result);
+    }
+}
