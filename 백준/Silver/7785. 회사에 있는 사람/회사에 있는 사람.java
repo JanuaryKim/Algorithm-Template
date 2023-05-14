6870e12f19bf2d5187a61473f6e55c64a90c2a14
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-05-14 14:01:16 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 884 ms, Memory: 63216 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -3,26 +3,23 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bf.readLine());
+
+        Set<String> set = new HashSet<>();
+        
+        for (int i = 0; i < n; i++) {
+            String[] line = bf.readLine().split(" ");
+            if(line[1].equals("enter"))
+                set.add(line[0]);
+            else
+                set.remove(line[0]);
+        }
+ 
         StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(br.readLine());
-        Set<String> set = new HashSet<String>();
-        for(int i=0;i<N;i++) {
-            String[] log = br.readLine().split(" ");
-            if(log[1].equals("enter")) {
-                set.add(log[0]);
-            }
-            else {
-                set.remove(log[0]);
-            }
-        }
-
-        ArrayList<String> list = new ArrayList<>(set);
-        Collections.sort(list, Collections.reverseOrder());
-        for(String s : list) {
-            sb.append(s+"\n");
-        }
+        set.stream().sorted(Collections.reverseOrder()).forEach(name-> sb.append(name+"\n"));
         System.out.println(sb);
     }
 }
