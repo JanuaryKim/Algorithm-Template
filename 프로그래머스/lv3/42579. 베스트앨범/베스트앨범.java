41a45732b0f9732629760ea564e8e0e1c2f61bfd
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-07-10 17:44:34 +0900

    [level 3] Title: 베스트앨범, Time: 1.11 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
@@ -1,49 +1,80 @@
 import java.util.*;
- 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < genres.length; i++) {
-            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
-        }
-        
 
-        ArrayList<String> genre = new ArrayList<>();
-        for(String key : map.keySet()) {
-            genre.add(key);
+        Map<String, Integer> genresScores = new HashMap<>(); //장르별 스트리밍 맵
+        Map<String, List<Song>> songsMap = new HashMap<>(); //각 장르별 노래 리스트 맵
+
+        for(int i = 0; i < genres.length; i++)
+        {
+            genresScores.put(genres[i], genresScores.getOrDefault(genres[i], 0) + plays[i]);
+
+            Song song = new Song();
+            song.number = i;
+            song.stmCtn = plays[i];
+            List<Song> songsList = songsMap.getOrDefault(genres[i], new ArrayList<>());
+            songsList.add(song);
+            songsMap.put(genres[i], songsList);
         }
-        Collections.sort(genre, (o1, o2) -> map.get(o2) - map.get(o1)); 
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0; i < genre.size(); i++) {
-            String g = genre.get(i);
-        
-            int max = 0;
-            int firstIdx = -1;
-            for(int j = 0; j < genres.length; j++) {
-                if(g.equals(genres[j]) && max < plays[j]) {
-                    max = plays[j];
-                    firstIdx = j;
+
+        List<String> descOrderGenresList = new ArrayList(); //장르별 스트리밍 내림차순 리스트
+        while(genresScores.size() > 0)
+        {
+            Set<String> keys = genresScores.keySet();
+            int maxScore = 0;
+            String maxKey = "";
+            for(String key : keys)
+            {
+                int score = genresScores.get(key);
+                if(maxScore < score)
+                {
+                    maxScore = score;
+                    maxKey = key;
                 }
             }
-  
-            max = 0;
-            int secondIdx = -1;
-            for(int j = 0; j < genres.length; j++) {
-                if(g.equals(genres[j]) && max < plays[j] && j != firstIdx) { 
-                    max = plays[j];
-                    secondIdx = j;
-                }
-            }
-            
-            list.add(firstIdx);
-            if(secondIdx >= 0) list.add(secondIdx);
+            descOrderGenresList.add(maxKey);
+            genresScores.remove(maxKey);
         }
         
-        int[] result = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
+        for(int i = 0; i < descOrderGenresList.size(); i++)
+        {
+            String genresName = descOrderGenresList.get(i);
+            List<Song> songsList = songsMap.get(genresName);
+            Collections.sort(songsList, (s1, s2) -> {
+                if(s1.stmCtn > s2.stmCtn)
+                    return -1;
+                else if(s1.stmCtn == s2.stmCtn)
+                    return 0;
+                else
+                    return 1;
+            });
         }
+
+        List<Integer> resultList = new ArrayList();
+        for(String key : descOrderGenresList)
+        {
+            List<Song> list = songsMap.get(key);
+            if(list.size() > 1) {
+                resultList.add(list.get(0).number);
+                resultList.add(list.get(1).number);
+            }
+            else
+            {
+                resultList.add(list.get(0).number);
+            }
+        }
+
+        int[] result = new int[resultList.size()];
+        for(int i = 0; i < result.length; i++)
+        {
+            result[i] = resultList.get(i);
+        }
+
         return result;
+    }
+
+    class Song{
+        public int number = -1;
+        public int stmCtn = 0;
     }
 }
