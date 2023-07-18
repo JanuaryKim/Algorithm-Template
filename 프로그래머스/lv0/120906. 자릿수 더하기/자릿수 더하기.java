bb9c8b7cfbfd240db2442672c96bc1e049900060
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-07-18 17:09:14 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int result = 0;
+        String str = String.valueOf(n);
+        for(int i = 0; i < str.length(); i++)
+        {
+            result += Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+        return result;
+    }
+}
