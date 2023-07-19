4142dbe7b3398698ae5e65295c97c0d1da0a742f
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-07-20 00:23:32 +0900

    [level 0] Title: 외계어 사전, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
@@ -1,20 +1,21 @@
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        int answer=0;
-        for(int i=0; i<dic.length; i++) {               
-            int check=0;                                
-            for(int j=0; j<spell.length; j++) {         
-                if(dic[i].indexOf(spell[j])==-1) {      
-                    check=-1;                           
-                    answer=2;                           
-                    break;                              
+        
+        for(int i = 0; i < dic.length; i++)
+        {
+            boolean check = true;
+            for(int j = 0; j < spell.length; j++)
+            {
+                if(!dic[i].contains(spell[j]))
+                {
+                    check = false;
+                    break;
                 }
             }
-            if(check==0) {
-                answer=1;
-                break;
-            }     
-    }
-        return answer;
+            if(check) 
+                return 1;
+        }
+        return 2;
     }
 }
+
