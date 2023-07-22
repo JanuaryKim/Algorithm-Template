b932ef855e8b0a2387f2c61b587cff32810342ca
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-07-22 23:39:07 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < my_string.length(); i++)
+        {
+            int idx = i;
+            if(i == num1)
+                idx = num2;
+            else if(i == num2)
+                idx = num1;
+            
+            sb.append(my_string.charAt(idx));
+        }
+        return sb.toString();
+    }
+}
