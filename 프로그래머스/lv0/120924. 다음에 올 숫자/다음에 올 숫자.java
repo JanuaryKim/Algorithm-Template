abe7a65aab9ca0c9934a6a527fcaeb54966b4c7d
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-07-26 16:19:50 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,24 +1,15 @@
 class Solution {
     public int solution(int[] common) {
         
-        int result = common[1] - common[0];
-        boolean check = true;
-        int diff = 0;
-        for(int i = 2; i < common.length; i++)
-        {
-            diff = common[i] - common[i-1];
-            if(result != diff) {
-                check = false;
-                
-                break;
-            }
-        }
-        if(check) {
-            return common[common.length-1] + diff;
+        int diff1 = common[1] - common[0];
+        int diff2 = common[2] - common[1];
+        
+        if(diff1 == diff2){
+            return common[common.length-1] + diff1;
         }
         else {
-            diff = common[common.length-1] / common[common.length-2];
-            return common[common.length-1] * diff;
+            int diffTemp = common[common.length-1] / common[common.length-2];
+            return common[common.length-1] * diffTemp;
         }
     }
 }
