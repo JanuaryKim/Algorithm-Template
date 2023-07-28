11ed0e3db0698938e3722b6eaf16239e4d8d3609
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-07-29 00:54:33 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -2,20 +2,11 @@ class Solution {
     public int solution(int a, int b, int n) {
         
         int result = 0;
-        int haveCoke = n; //20
-        while(haveCoke >= a)
+        while(n >= a)
         {
-            int given = 0;
-            while(haveCoke >= a) 
-            {
-                haveCoke -= a; 
-                given += b; 
-            }
-            
-            result += given; 
-            haveCoke += given; 
+            result += b * (n / a);
+            n = b * (n / a) + n % a;
         }
-        
         return result;
     }
 }
