5321291aab8957f43c6f3a16ec850b05ebbe2afc
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-08-03 16:20:58 +0900

    [level 0] Title: OX퀴즈, Time: 0.11 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -2,17 +2,37 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] quiz) {
-     
-        ArrayList<String> answerList = new ArrayList<>();
-        for (String q : quiz) {
-            String[] quizSplit = q.replaceAll("- -", "").replaceAll("- ", "-").replaceAll("[+] ", "").split(" ");
-            if (Integer.parseInt(quizSplit[0]) + Integer.parseInt(quizSplit[1]) == Integer.parseInt(quizSplit[3])) {
-                answerList.add("O");
-            } else {
-                answerList.add("X");
+        
+        String[] result = new String[quiz.length];
+        
+        for(int i = 0; i < quiz.length; i++)
+        {
+            String[] spl = quiz[i].split("=");
+            String[] subSpl = spl[0].split(" ");
+            
+            int sum = Integer.parseInt(subSpl[0]);
+            String s = ".";
+            for(int j = 1; j < subSpl.length; j++)
+            {
+                String curS = subSpl[j];
+                // System.out.println(curS);
+                if(curS.equals("-") || curS.equals("+"))
+                    s = curS;
+                else
+                {
+                    if(s.equals("-"))
+                        sum = sum - Integer.parseInt(curS);
+                    else
+                        sum = sum + Integer.parseInt(curS);
+                }
             }
+            
+            if(sum == Integer.parseInt(spl[1].trim()))
+                result[i] = "O";
+            else
+                result[i] = "X";
         }
-        String[] answer = answerList.toArray(new String[0]);
-        return answer;
+        
+        return result;
     }
 }
