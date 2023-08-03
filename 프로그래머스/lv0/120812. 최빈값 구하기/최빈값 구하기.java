f8e980d87c4ea0d310b8e0529369c968ca852e3e
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-08-03 17:05:28 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.07 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -3,27 +3,26 @@ import java.util.*;
 class Solution {
     public int solution(int[] array) {
         
-        Map<Integer,Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < array.length; i++)
-            map.put(array[i], map.getOrDefault(array[i], 0) + 1);
-        
+        int[] arr = new int [1000];
         int maxNum = 0;
         int repeatCtn = 0;
-        boolean isEqual = false;
-        for(Integer num : map.keySet())
-        {
-            int ctn = map.get(num);
-            if(ctn > repeatCtn)
-            {
-                maxNum = num;
-                repeatCtn = ctn;
-                isEqual = false;
-            }
-            else if(ctn == repeatCtn)
-                isEqual = true;
-        }
+        boolean isEquals = false;
+        for(int i = 0; i < array.length; i++)
+            arr[array[i]]++;
         
-        return isEqual ? -1 : maxNum;
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] > repeatCtn)
+            {
+                maxNum = i;
+                repeatCtn = arr[i];
+                isEquals = false;
+            }
+            else if(arr[i] == repeatCtn)
+            {
+                isEquals = true;
+            }
+        }
+        return isEquals ? -1 : maxNum;
     }
 }
