f8bcef3469c6294da4af7217bb8914d8b55e383e
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-08-06 00:34:58 +0900

    [unrated] Title: 문자열 나누기, Time: 0.69 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int solution(String s) {
+        
+        int answer = 0;
+        int xCtn = 0;
+        int otherCtn = 0;
+        char firstC = '-';
+        
+        for(int i = 0; i < s.length(); i ++)
+        {
+            char curC = s.charAt(i);
+            
+            if(firstC == '-') {
+                firstC = curC;
+                xCtn++;
+                continue;
+            }
+            
+            if(firstC == curC)
+            {
+                xCtn++;
+            }
+            else
+            {
+                otherCtn++;
+            }
+            
+            if(xCtn == otherCtn)
+            {
+                answer++;
+                firstC = '-';
+                xCtn = 0;
+                otherCtn = 0;
+            }          
+        }
+        
+        if(firstC != '-')
+            answer++;
+        
+        return answer;
+    }
+}
