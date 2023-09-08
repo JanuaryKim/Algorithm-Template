7479a9bc036c8c89b37651a56b696ad68ca97bf8
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-09-08 23:33:07 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,9 +1,3 @@
 function solution(seoul) {
-    
-    let result = 0;
-    seoul.filter((name,index) => {
-        if(name === "Kim")
-            result = index;
-    })
-    return `김서방은 ${result}에 있다`;
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
 }
