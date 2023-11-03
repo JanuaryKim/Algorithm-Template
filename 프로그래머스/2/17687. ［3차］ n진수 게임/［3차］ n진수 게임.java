60db3f1d43b2b6ac9f6c6e00ec336b9762fb8c9b
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2023-11-04 00:09:18 +0900

    [level 2] Title: [3차] n진수 게임, Time: 21.67 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -12,16 +12,13 @@ class Solution {
             String strCurNum = Integer.toString(curNum++, n).toUpperCase();
             System.out.println(strCurNum);
             for(int i = 0; i < strCurNum.length(); i++){
-                
-                // [0, 1, [1, 0, [1, 1, [1, 0, 0
-                char c = strCurNum.charAt(i);
-                
+
+                char c = strCurNum.charAt(i);                
                 if(turn == p){
                     sb.append(c);
-                    if(sb.length() == t)
+                    if(sb.length() == t) //이미 구할 갯수를 다 구했는데도 for문의 반복으로 인해 추가될 경우 대비, 중복 코드 발생
                         break;
                 }
-                
                 turn++;
                 if(turn > m){
                     turn = 1;
