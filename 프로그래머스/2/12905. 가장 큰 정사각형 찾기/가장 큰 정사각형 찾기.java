481230dac41081cc3029807277005a52d7d0406a
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2024-01-20 20:10:00 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 16.09 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,30 +1,34 @@
+import java.util.*;
 class Solution
 {
     public int solution(int [][]board)
-    {
-        int answer = 0;
-        
-        int[][] dp = new int[board.length][board[0].length];
-
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                dp[i][j] = board[i][j];
-                if(board[i][j] == 1){
-                    answer = 1;
+    {      
+        int result = 1;
+        boolean isInLogic = false;
+        for(int i = 1; i < board.length; i++){
+            for(int j = 1; j < board[i].length; j++){
+                if(board[i][j] == 0) continue;
+                if(j >= board[i-1].length) continue; //현재 행의 열 인덱스 보다 이전 행의 길이가 짧을 경우 
+                isInLogic = true;
+                board[i][j] = Math.min(Math.min(board[i][j-1], board[i-1][j]) , board[i-1][j-1]) + 1;
+                if(board[i][j] > result) result = board[i][j];
+            }
+        }
+        if(!isInLogic){
+            result = 0;
+            for(int i = 0; i < board.length; i++){
+                for(int j = 0; j < board[i].length; j++){
+                    if(result < board[i][j]) {
+                        result = board[i][j];
+                        break;
+                    }
                 }
             }
         }
-        
-        for(int i = 1; i < dp.length; i++){
-            for(int j = 1; j < dp[0].length; j++){
-                if(dp[i][j] != 0){
-                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]) , dp[i-1][j-1]) + 1;
-                    answer = Math.max(answer, dp[i][j] * dp[i][j]);
-                }
-            }
-        }
-        
-        
-        return answer;
+        return result * result;
     }
 }
+
+//[1], 
+//[1, 1], 
+//[1, 1, 1]
