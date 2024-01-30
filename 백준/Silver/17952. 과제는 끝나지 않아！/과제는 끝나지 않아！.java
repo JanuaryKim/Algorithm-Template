fca4b19cc05ced69a92864a17eba3ed6b1b43dc6
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2024-01-30 17:34:32 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 1164 ms, Memory: 313176 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
@@ -10,36 +10,26 @@ public class Main {
         int result = 0;
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
-        int[][] works = new int[n][3];
-
-        for(int i = 0; i < n; i++){
-            int[] work = Arrays.stream(bf.readLine().split(" ")).mapToInt(s->Integer.parseInt(s)).toArray();
-            works[i] = work;
-        }
 
         Stack<int[]> stack = new Stack<>();
-        int time = 0;
-        while(time < n){
-            time++;
-
-            if(works[time-1][0] != 0){
-                int[] w = works[time-1];
-                w[2] -= 1;
-                if(w[2] == 0) {
-                    result += w[1];
+        for(int i = 0; i < n; i++){
+            int[] work = Arrays.stream(bf.readLine().split(" ")).mapToInt(s->Integer.parseInt(s)).toArray();
+            if(work[0] == 1){
+                work[2]--;
+                if(work[2] == 0){
+                    result += work[1];
                     continue;
                 }
-                stack.push(w);
-            }
-            else{
-                if(!stack.isEmpty()){
+                stack.push(work);
+            } else{
+                if (!stack.isEmpty()) {
                     int[] w = stack.pop();
-                    w[2] -= 1;
+                    w[2]--;
                     if(w[2] == 0){
                         result += w[1];
+                        continue;
                     }
-                    else
-                        stack.push(w);
+                    stack.push(w);
                 }
             }
         }
