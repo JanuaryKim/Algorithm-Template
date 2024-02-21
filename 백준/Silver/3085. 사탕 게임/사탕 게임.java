a1f4afb6ad0d7324215ef8dd4c0526c640812cf9
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2024-02-21 15:59:40 +0900

    [Silver II] Title: 사탕 게임, Time: 328 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,84 +1,77 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     static char[][] board;
-    static boolean[][] visited;
     static int n;
     static int maxContinue = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bf.readLine());
         board = new char[n][n];
-        visited = new boolean[n][n];
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             String line = bf.readLine();
-            for(int j = 0; j < n; j++){
+            for (int j = 0; j < n; j++) {
                 board[i][j] = line.charAt(j);
             }
         }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 //열 교환
-                if(j + 1 < n) {
-                    swap(i,j,i,j+1);
+                if (j + 1 < n) {
+                    swap(i, j, i, j + 1);
                     checkContinueLine();
-                    swap(i,j,i,j+1);
+                    swap(i, j, i, j + 1);
                 }
 
                 //행 교환
-                if(i + 1 < n){
-                    swap(i,j,i+1,j);
+                if (i + 1 < n) {
+                    swap(i, j, i + 1, j);
                     checkContinueLine();
-                    swap(i,j,i+1,j);
+                    swap(i, j, i + 1, j);
                 }
             }
         }
         System.out.println(maxContinue);
     }
 
-    private static void swap(int y1, int x1, int y2, int x2){
+    private static void swap(int y1, int x1, int y2, int x2) {
         char temp = board[y1][x1];
         board[y1][x1] = board[y2][x2];
         board[y2][x2] = temp;
     }
 
-    private static int checkContinueLine(){
+    private static int checkContinueLine() {
         int result = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 char c = board[i][j];
                 int tempMax = 0;
-                if(!visited[i][j]){
-                    //열 확인
-                    for(int z = j; z < n; z++){
-                        if(board[i][z] == c)
-                            tempMax++;
-                        else
-                            break;
-                    }
-
-                    maxContinue = Math.max(maxContinue, tempMax);
-                    tempMax = 0;
-                    
-                    //행 확인
-                    for(int z = i; z < n; z++){
-                        if(board[z][j] == c) {
-                            tempMax++;
-                        }
-                        else
-                            break;
-                    }
-                    maxContinue = Math.max(maxContinue, tempMax);
+                //열 확인
+                for (int z = j; z < n; z++) {
+                    if (board[i][z] == c)
+                        tempMax++;
+                    else
+                        break;
                 }
+
+                maxContinue = Math.max(maxContinue, tempMax);
+                tempMax = 0;
+
+                //행 확인
+                for (int z = i; z < n; z++) {
+                    if (board[z][j] == c) {
+                        tempMax++;
+                    } else
+                        break;
+                }
+                maxContinue = Math.max(maxContinue, tempMax);
             }
         }
-
         return result;
     }
 }
