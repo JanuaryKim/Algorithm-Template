77d8c7ae73a4ea67cdb34bcb94a6608f6a9b7e83
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2024-03-03 00:00:33 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 132 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -8,35 +8,34 @@ public class Main {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String oriWord = bf.readLine();
-        String newWord = "";
+
+        StringBuilder newWord = new StringBuilder();
         for(int i = 0; i < oriWord.length(); i++){
-            newWord += oriWord.charAt(i);
+            newWord.append(oriWord.charAt(i));
             if(i != oriWord.length()-1)
-                newWord+=" ";
+                newWord.append(" ");
         }
 
         int middle = newWord.length() / 2;
         int left = middle - 1;
         int right = middle + 1;
-        while(true){
+        while(true) {
 
-            if(left  < 0 && right > newWord.length()-1)
+            if (left < 0 && right > newWord.length() - 1)
                 break;
-            else if(right > newWord.length() - 1){
-                newWord += newWord.charAt(left);
+            else if (right > newWord.length() - 1) {
+                newWord.append(newWord.charAt(left));
             }
 
-            if(newWord.charAt(left) != newWord.charAt(right)){
+            if (newWord.charAt(left) != newWord.charAt(right)) {
                 middle++;
                 left = middle - 1;
                 right = middle + 1;
-            }
-            else{
+            } else {
                 left--;
                 right++;
             }
         }
-        newWord = newWord.replaceAll(" ", "");
-        System.out.println(newWord.length());
+        System.out.println(new String(newWord).replaceAll(" ", "").length());
     }
 }
