4e8d38d5b7b43710ed552fa7cc9fee32e83e630e
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2024-03-14 00:48:28 +0900

    [Silver II] Title: 꽃길, Time: 212 ms, Memory: 25876 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.java
+++ 백준/Silver/14620. 꽃길/꽃길.java
@@ -1,64 +1,107 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int[][]matrix;
-	static int N;
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		matrix = new int[N][N];
-		int answer = Integer.MAX_VALUE;
-		
-		for (int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j=0; j<N; j++) {
-				matrix[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		for (int i=0; i<(N*N); i++) {
-			for (int j=0; j<(N*N); j++) {
-				for (int k=0; k<(N*N); k++) {
-					ArrayList<Integer> list = new ArrayList<>();
-					list.add(i);
-					list.add(j);
-					list.add(k);
-					answer = Math.min(answer, findCost(list));
-				}	
-			}
-		}
-		
-		System.out.println(answer);
-	}
-	static int findCost(ArrayList<Integer> list) {
-		int[] dx = {0, -1, 1, 0, 0};
-		int[] dy = {0, 0, 0, -1, 1};	
-		boolean[][] check = new boolean[N][N];
-		
-		int result = 0;
-		for (int i: list) {
-			int x = i / N;
-			int y = i % N;
-			
-			if (x==0 || x==(N-1) || y==0 || y==(N-1)) {
-				return 10000;
-			}
-			
-			for (int j=0; j<5; j++) {
-				if (!check[x+dx[j]][y+dy[j]]) {
-					check[x+dx[j]][y+dy[j]] = true;
-					result += matrix[x+dx[j]][y+dy[j]];
-				}
-				else return 10000;				
-			}
-		}
-		return result;
-	}
+    static int[][] map;
+    static int minCost = Integer.MAX_VALUE;
+    static boolean[][] visited;
+    static Point[] seedCenter = new Point[3];
+    static int n;
+    static int[] cy = {-1, 1, 0, 0}; //상, 하, 좌, 우
+    static int[] cx = {0,0, -1, 1}; //상, 하, 좌, 우
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        map = new int[n][n];
+        visited = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] splLine = bf.readLine().split(" ");
+            for(int j = 0; j < n; j++){
+                map[i][j] = Integer.parseInt(splLine[j]);
+            }
+        }
+
+        dfs(0,  0);
+
+        System.out.println(minCost);
+
+    }
+
+    private static void dfs(int seedIdx, int curCost) {
+
+
+        if(seedIdx >= 3){
+            if(curCost < minCost){
+                minCost = curCost;
+            }
+            return;
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(checkPossiblePlant(i,j)){ //심기 가능한가?
+
+                    int cost = plant(true, seedIdx,i, j);
+                    dfs(seedIdx+1, curCost + cost);
+                    plant(false, seedIdx, i, j);
+                    //심는다. (visited 체크), 중심좌표 저장
+                    //재귀
+                    //뽑는다. (visited 체크 해제), 중신좌표 지우기
+                }
+
+            }
+        }
+    }
+
+    private static boolean checkPossiblePlant(int y, int x) {
+
+//        System.out.println("체킹 : "  + y + " , " + x);
+        boolean result = true;
+
+        if(y >= n || x >= n || y < 0 || x < 0|| visited[y][x])
+        {
+
+            return false;
+        }
+
+        for(int i = 0; i < 4; i++){
+            int aroundY = y + cy[i];
+            int aroundX = x + cx[i];
+
+            if(aroundY >= n || aroundX >= n || aroundY < 0 || aroundX < 0 || visited[aroundY][aroundX]){
+                return false;
+            }
+        }
+
+        return result;
+    }
+
+    private static int plant(boolean plant, int seedIdx, int y, int x) {
+
+//        System.out.println(seedIdx + " 번째 "+ y + " , " + x);
+        int cost = 0;
+
+        visited[y][x] = plant;
+        cost += map[y][x];
+        for(int i = 0; i < 4; i++){
+            int aroundY = y + cy[i];
+            int aroundX = x + cx[i];
+
+            visited[aroundY][aroundX] = plant;
+            cost += map[aroundY][aroundX];
+        }
+        seedCenter[seedIdx] = new Point(y,x);
+        return cost;
+    }
+
+
+    private static class Point{
+        int y, x;
+        public Point(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
 }
