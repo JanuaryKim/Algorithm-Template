e0a4d932997f8f6ec1ad0d49f71a8bfa6308c21e
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2024-03-14 12:36:51 +0900

    [Silver III] Title: 카드 문자열, Time: 324 ms, Memory: 30668 KB -BaekjoonHub

--- 백준/Silver/13417. 카드 문자열/카드 문자열.java
+++ 백준/Silver/13417. 카드 문자열/카드 문자열.java
@@ -3,17 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Deque;
 import java.util.LinkedList;
-
+//최초 카드는 일단 가져오고, 2번째 가져오는 카드부터는 현재 놓여진 카드의 맨 왼쪽의 카드의 문자와 비교하여 사전순으로 빠르거나 같으면 왼쪽 아니면 오른쪽에 추가
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         int t = Integer.parseInt(bf.readLine());
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < t; i++) {
             int n = Integer.parseInt(bf.readLine());
             String[] splCards = bf.readLine().split(" ");
-
             Deque<String> dq = new LinkedList<>();
             dq.add(splCards[0]);
             for (int j = 1; j < n; j++) {
@@ -25,8 +23,6 @@ public class Main {
             dq.stream().forEach(s->sb.append(s));
             if(i != t -1) sb.append("\n");
         }
-
         System.out.println(sb);
     }
-
 }
