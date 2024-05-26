c2462cf0a75b2f2babd44d820b21e9e97fe932d8
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2024-05-26 11:21:17 +0900

    [Silver III] Title: 에너지 드링크, Time: 384 ms, Memory: 30636 KB -BaekjoonHub

--- 백준/Silver/20115. 에너지 드링크/에너지 드링크.java
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.java
@@ -6,19 +6,17 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         double result = 0;
+        long maxDrink = 0;
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
         Long[] drinks = Arrays.stream(bf.readLine().split(" ")).mapToLong(Long::parseLong).boxed().toArray(Long[]::new);
-        Arrays.sort(drinks, (d1, d2) -> {
-            return d2 > d1 ? 1 : (d2 < d1 ? -1 : 0);
-        });
 
-        result += drinks[0];
-
-        for(int i = drinks.length-1; i > 0; i--){
-            result += ((double)drinks[i] / 2);
+        for(int i = 0; i < drinks.length; i++){
+            if(maxDrink < drinks[i]) maxDrink = drinks[i];
+            result += drinks[i];
         }
-        System.out.println(result);
 
+        System.out.println((result+maxDrink) /2);
     }
 }
+    
