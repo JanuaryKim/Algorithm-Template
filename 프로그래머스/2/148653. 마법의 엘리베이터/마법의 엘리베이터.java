63e900b451bad83d18a04a544de9d18c63e58113
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2024-06-01 11:59:02 +0900

    Update 마법의 엘리베이터.java

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -4,8 +4,8 @@ class Solution {
         int answer = 0;
         
         while(storey != 0){
-            int one = storey % 10;
-            int ten = (storey / 10) % 10;
+            int one = storey % 10; //일의 자리
+            int ten = (storey / 10) % 10; //십의 자리, 일의자리가 5일 경우에를 위해 필요
             if(one < 5) {
                 storey -= one;
                 answer += one;
@@ -15,7 +15,7 @@ class Solution {
                 answer += 10 - one;
             }
             else{
-                if(ten >= 5)
+                if(ten >= 5) //십의 자리가 5 이상일 경우 더해줘야 함.
                 {
                     storey += 10 - one;
                     answer += 10 - one;
