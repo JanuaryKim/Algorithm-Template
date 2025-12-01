fb237d798d37a0382e861a10b75a11ceaee705f2
JanuaryKim <107736531+JanuaryKim@users.noreply.github.com>
2025-12-01 23:40:52 +0900

    [Bronze II] Title: 풀만한문제, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/29716. 풀만한문제/풀만한문제.py
+++ 백준/Bronze/29716. 풀만한문제/풀만한문제.py
@@ -1,21 +1,26 @@
-J, N = map(int ,input().split())
 
-cnt = 0
 
-for i in range(0, N) : 
-    A = input()
-    
-    sum = 0
-    
-    for j in range(0, len(A)) : 
-        if A[j].isupper() : 
-            sum += 4
-        elif A[j].islower() or A[j].isdigit(): 
-            sum += 2
-        elif A[j] == ' ' : 
-            sum += 1
-    
-    if J >= sum : 
-        cnt += 1
+J, N = map(int, input().split())
 
-print(cnt)
+result = 0
+
+Q = []
+
+for i in range(0, N):
+    tempQ = input()
+    Q.append(tempQ)
+
+for i in Q:
+    tempSum = 0
+    for j in range(0, len(i)):
+
+        if i[j].isupper():
+            tempSum += 4
+        elif i[j].islower() or i[j].isdigit():
+            tempSum += 2
+        else:
+            tempSum += 1
+    if tempSum <= J:
+        result += 1
+
+print(result)
